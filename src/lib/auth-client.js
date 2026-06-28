6e7a524844e1f8a70974e458df96fa4dad4d4f71
G. <rmgolamrabbani@gmail.com>
2026-06-28 20:26:29 +0600

    change better auth url

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { createAuthClient } from "better-auth/react"
 import { inferAdditionalFields, jwtClient } from "better-auth/client/plugins" 
 
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+    baseURL: process.env.BETTER_AUTH_URL,
     plugins: [
         inferAdditionalFields({
             user: {
